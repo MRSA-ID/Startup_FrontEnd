d15d17198fb628ab8843d3c569a182ffd4633219
MRSA <sanggisda@gmail.com>
2023-08-06 22:35:40 +0700

    for deploy

---
+++ .nuxt/components/index.js
@@ -0,0 +1,38 @@
+export const CallToAction = () => import('../..\\components\\CallToAction.vue' /* webpackChunkName: "components/call-to-action" */).then(c => wrapFunctional(c.default || c))
+export const Footer = () => import('../..\\components\\Footer.vue' /* webpackChunkName: "components/footer" */).then(c => wrapFunctional(c.default || c))
+export const Navbar = () => import('../..\\components\\Navbar.vue' /* webpackChunkName: "components/navbar" */).then(c => wrapFunctional(c.default || c))
+export const NuxtLogo = () => import('../..\\components\\NuxtLogo.vue' /* webpackChunkName: "components/nuxt-logo" */).then(c => wrapFunctional(c.default || c))
+export const Tutorial = () => import('../..\\components\\Tutorial.vue' /* webpackChunkName: "components/tutorial" */).then(c => wrapFunctional(c.default || c))
+export const IconsCloseIcon = () => import('../..\\components\\icons\\CloseIcon.vue' /* webpackChunkName: "components/icons-close-icon" */).then(c => wrapFunctional(c.default || c))
+export const IconsMenuIcon = () => import('../..\\components\\icons\\MenuIcon.vue' /* webpackChunkName: "components/icons-menu-icon" */).then(c => wrapFunctional(c.default || c))
+
+// nuxt/nuxt.js#8607
+function wrapFunctional(options) {
+  if (!options || !options.functional) {
+    return options
+  }
+
+  const propKeys = Array.isArray(options.props) ? options.props : Object.keys(options.props || {})
+
+  return {
+    render(h) {
+      const attrs = {}
+      const props = {}
+
+      for (const key in this.$attrs) {
+        if (propKeys.includes(key)) {
+          props[key] = this.$attrs[key]
+        } else {
+          attrs[key] = this.$attrs[key]
+        }
+      }
+
+      return h(options, {
+        on: this.$listeners,
+        attrs,
+        props,
+        scopedSlots: this.$scopedSlots,
+      }, this.$slots.default)
+    }
+  }
+}
