ed975fa0a8ffae79f852345f2d3de233807b0153
MRSA <sanggisda@gmail.com>
2023-08-06 23:11:51 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+let development = process.env.NODE_ENV !== 'production';
+
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
@@ -51,7 +53,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:8080/',
+    baseURL: development ? 'http://localhost:8080/' : process.env.BASE_URL,
   },
 
   auth: {
