48c3e7c02d25e7f9dd4808413949d3d4e73a9246
MRSA <sanggisda@gmail.com>
2023-08-07 13:25:26 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,6 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: development ? 'http://localhost:8080/' : process.env.BASE_URL,
-    proxy: true,
     credentials: false
   },
 
